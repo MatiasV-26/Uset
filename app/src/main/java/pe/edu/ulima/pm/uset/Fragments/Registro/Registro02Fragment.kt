156618fc561b757840be1eef126559d773d90136
Matias Villanueva <matias.villanueva.paz@gmail.com>
2022-07-13 23:06:10 -0500

    inicio

---
+++ app/src/main/java/pe/edu/ulima/pm/uset/Fragments/Registro/Registro02Fragment.kt
@@ -0,0 +1,26 @@
+package pe.edu.ulima.pm.uset.Fragments.Registro
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pe.edu.ulima.pm.uset.R
+
+class Registro02Fragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registro02, container, false)
+    }
+
+}
