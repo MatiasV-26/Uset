c81408149528fe057c0724818e05d5ee306e817d
Marco Antonio Landauro Alba <20191063@aloe.ulima.edu.pe>
2022-07-16 15:12:31 -0500

    Merge 16

--- app/src/main/java/pe/edu/ulima/pm/uset/CreateProfileActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/uset/CreateProfileActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class CreateProfileActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_profile)
     }
+
 }
--- app/src/main/java/pe/edu/ulima/pm/uset/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/uset/MainActivity.kt
@@ -2,13 +2,6 @@ package pe.edu.ulima.pm.uset
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-
-private lateinit var auth : FirebaseAuth
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
