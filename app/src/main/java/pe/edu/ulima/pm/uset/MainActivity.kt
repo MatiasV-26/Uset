8ae0760d2e56012496abb65fdd0721ec6e4db704
Franco <franco1.alfra@gmail.com>
2022-07-16 13:18:59 -0500

    Firebase

--- app/src/main/java/pe/edu/ulima/pm/uset/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/uset/MainActivity.kt
@@ -2,10 +2,18 @@ package pe.edu.ulima.pm.uset
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+private lateinit var auth : FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
