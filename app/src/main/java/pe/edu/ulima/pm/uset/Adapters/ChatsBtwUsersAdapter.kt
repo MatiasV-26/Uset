737ef70a4128d9f77d603d36bfe5300e6f4a4bc6
Franco <franco1.alfra@gmail.com>
2022-07-17 03:23:04 -0500

    funcionalidad chats basica hecha

--- app/src/main/java/pe/edu/ulima/pm/uset/Adapters/ChatsBtwUsersAdapter.kt
+++ app/src/main/java/pe/edu/ulima/pm/uset/Adapters/ChatsBtwUsersAdapter.kt
@@ -6,13 +6,14 @@ import androidx.recyclerview.widget.RecyclerView
 import pe.edu.ulima.pm.uset.Models.MessageChat
 import pe.edu.ulima.pm.uset.R
 
-class ChatsBtwUsersAdapter:RecyclerView.Adapter<ChatsBtwUsersHolder>() {
+class ChatsBtwUsersAdapter(private val userId : String):RecyclerView.Adapter<ChatsBtwUsersHolder>() {
 
-    var a = MessageChat("Mensaje1","DASDADASD")
-    var b = MessageChat("Mensaje2","DASDADASD")
-    var c = MessageChat("Mensaje3","DASDADASD")
+    var messageList: List<MessageChat> = listOf()
 
-    var messageList: List<MessageChat> = listOf(a,b,c)
+    fun setData(messages : List<MessageChat>){
+        messageList = messages
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup
                                     , viewType: Int
@@ -26,7 +27,7 @@ class ChatsBtwUsersAdapter:RecyclerView.Adapter<ChatsBtwUsersHolder>() {
     override fun onBindViewHolder(holder: ChatsBtwUsersHolder, position: Int) {
         val msg = messageList[position]
 
-        holder.render(msg)
+        holder.render(msg,userId)
     }
 
     override fun getItemCount(): Int = messageList.size
--- app/src/main/java/pe/edu/ulima/pm/uset/ChatActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/uset/ChatActivity.kt
@@ -1,7 +1,11 @@
 package pe.edu.ulima.pm.uset
 
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import pe.edu.ulima.pm.uset.Fragments.AddFriends.AddFriendFragment
 import pe.edu.ulima.pm.uset.Fragments.Chats.ChatListUsers
 import pe.edu.ulima.pm.uset.databinding.ActivityChatBinding
 
@@ -10,7 +14,7 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChatBinding
 
     val fragmentManager = supportFragmentManager
-    val fragmentTransaction = fragmentManager.beginTransaction()
+    var fragmentTransaction = fragmentManager.beginTransaction()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +25,49 @@ class ChatActivity : AppCompatActivity() {
 
         title = "Chats"
 
+        val toggle = ActionBarDrawerToggle(this
+            ,binding.drawerMenuLayout
+            ,R.string.drawer_open
+            ,R.string.drawer_close)
+
+        binding.drawerMenuLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
         fragmentTransaction
             .add(R.id.fragmentContainerViewChats,ChatListUsers(),"listUsers")
             .addToBackStack("listUsers")
             .commit()
+
+        binding.navView.setNavigationItemSelectedListener {
+            it.setChecked(true)
+            when(it.itemId){
+                R.id.add_friend ->pressAddFriend()
+            }
+
+            binding.drawerMenuLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+    private fun pressAddFriend(){
+        Log.d("adadasda",supportFragmentManager.primaryNavigationFragment.toString())
+        fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction
+            .replace(R.id.fragmentContainerViewChats,AddFriendFragment(),"addFriends")
+            .addToBackStack("addFriendFragment")
+            .commit()
+        title = "Agregar amigos"
+    }
+
+    override fun onBackPressed() {
+        if(binding.drawerMenuLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerMenuLayout.closeDrawer(GravityCompat.START)
+        }else{
+            var a = binding.navView.checkedItem
+            if(a!=null){
+                a.setChecked(false)
+            }
+            super.onBackPressed()
+        }
     }
 }
