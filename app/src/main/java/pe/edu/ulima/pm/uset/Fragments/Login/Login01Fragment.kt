c4ed7a1aa4000e5d1e0f36a3a19309cfeba1ad4b
Marco Antonio Landauro Alba <20191063@aloe.ulima.edu.pe>
2022-07-17 13:20:59 -0500

    Auth for Loggin working

--- app/src/main/java/pe/edu/ulima/pm/uset/Fragments/Login/Login01Fragment.kt
+++ app/src/main/java/pe/edu/ulima/pm/uset/Fragments/Login/Login01Fragment.kt
@@ -7,48 +7,55 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import pe.edu.ulima.pm.uset.databinding.FragmentLogin01Binding
 
 class Login01Fragment : Fragment() {
 
     private var _binding: FragmentLogin01Binding? = null
     private val binding get() = _binding!!
+    // FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
     }
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentLogin01Binding.inflate(inflater, container, false)
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tvOlvideMiContrasena.setOnClickListener { TvOlvideMiContrasena() }
-        binding.btnIngresar.setOnClickListener{ BtnIngresar() }
-        binding.btnFacebook.setOnClickListener { BtnFacebook() }
-        binding.btnGoogle.setOnClickListener { BtnGoogle() }
-        binding.tvCrearUnaCuenta.setOnClickListener { TVCrearUnaCuenta() }
+        binding.tvOlvideMiContrasena.   setOnClickListener  { TvOlvideMiContrasena()    }
+        binding.btnIngresar.            setOnClickListener  { BtnIngresar()             }
+        binding.btnFacebook.            setOnClickListener  { BtnFacebook()             }
+        binding.btnGoogle.              setOnClickListener  { BtnGoogle()               }
+        binding.tvCrearUnaCuenta.       setOnClickListener  { TVCrearUnaCuenta()        }
     }
 
-    //TODO: IMPLEMENTAR TODAS LAS FUNCIONES DE ABAJO
+    //TODO: OnViewCreated Functions ****************************************************************
     private fun TvOlvideMiContrasena() {
         Toast.makeText(context, "Implementacion futura ...", Toast.LENGTH_SHORT).show()
     }
     private fun BtnIngresar() {
-        if (binding.tilCorreoEditText.text.toString().isNotEmpty()
-            &&
-            binding.tilIngresarContrasenaEditText.text.toString().isNotEmpty()){
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(
-                binding.tilCorreoEditText.text.toString(),
-                binding.tilIngresarContrasenaEditText.text.toString()
-            ).addOnCompleteListener{
-                if(it.isSuccessful){
-                    Toast.makeText(context, "LAS CREDENCIALES SON CORRECTAS", Toast.LENGTH_SHORT).show()
-                }else{
-                    Toast.makeText(context, "LAS CREDENCIALES SON MALAS :(", Toast.LENGTH_SHORT).show()
+        val email = binding.tilCorreoEditText.text.toString().trim { it <= ' ' }
+        val password = binding.tilIngresarContrasenaEditText.text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()){
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity!!){
+                if(it.isSuccessful) {
+                    Toast.makeText(context, "LAS CREDENCIALES SON CORRECTAS", Toast.LENGTH_SHORT)
+                        .show()
+                    val user = auth.currentUser
+                    updateUI(user)
+                }else {
+                    Toast.makeText(context, "LAS CREDENCIALES SON MALAS :(", Toast.LENGTH_SHORT)
+                        .show()
+                    updateUI(null)
                 }
             }
         }
@@ -62,7 +69,26 @@ class Login01Fragment : Fragment() {
     private fun TVCrearUnaCuenta() {
         Toast.makeText(context, "TVCrearUnaCuenta", Toast.LENGTH_SHORT).show()
     }
-    //TODO: IMPLEMENTAR TODAS LAS FUNCIONES DE ARRIBA
+
+    private fun updateUI(user: FirebaseUser?) {
+        Toast.makeText(context, user.toString(), Toast.LENGTH_SHORT).show()
+    }
+    //TODO: onViewCreated Functions ****************************************************************
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    //TODO: onStart Functions **********************************************************************
+    private fun reload() {
+
+    }
+    //TODO: onStart Functions **********************************************************************
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/pe/edu/ulima/pm/uset/LoginActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/uset/LoginActivity.kt
@@ -32,7 +32,6 @@ class LoginActivity : AppCompatActivity() {
     private fun SetFragmentTransactionInTop(fragment: Int) {
         fragmentTransaction
             .add(R.id.fragmentContainerViewLogin, fragmentLoginList[fragment],"1")
-            .addToBackStack("dasdasd")
             .commit()
     }
 
