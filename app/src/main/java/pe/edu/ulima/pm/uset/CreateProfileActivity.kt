83f878c932cdce2751bf93263989b4f3df7fdfac
Matias Villanueva <matias.villanueva.paz@gmail.com>
2022-07-18 01:26:47 -0500

    MARCO JUEGA xd

---
+++ app/src/main/java/pe/edu/ulima/pm/uset/CreateProfileActivity.kt
@@ -0,0 +1,30 @@
+package pe.edu.ulima.pm.uset
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import pe.edu.ulima.pm.uset.Fragments.CreateProfile.CreateProfile01Fragment
+import pe.edu.ulima.pm.uset.Fragments.Login.Login01Fragment
+import pe.edu.ulima.pm.uset.databinding.ActivityCreateProfileBinding
+import pe.edu.ulima.pm.uset.databinding.ActivityLoginBinding
+
+class CreateProfileActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityCreateProfileBinding
+    private val fragmentManager = supportFragmentManager
+    private val fragmentTransaction = fragmentManager.beginTransaction()
+    private val fragmentLoginList: List<Fragment> = listOf(CreateProfile01Fragment())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCreateProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        title = "...USET..."
+        SetFragmentTransactionInTop(0)
+    }
+
+    private fun SetFragmentTransactionInTop(fragment: Int) {
+        fragmentTransaction
+            .add(R.id.fragmentContainerViewCreateProfile, fragmentLoginList[fragment],"1")
+            .commit()
+    }
+}
