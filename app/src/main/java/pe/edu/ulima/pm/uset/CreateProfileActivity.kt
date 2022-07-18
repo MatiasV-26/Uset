bfd8dfe1543f69e30725eac3b87e5cfe6d18ffe4
Marco Antonio Landauro Alba <20191063@aloe.ulima.edu.pe>
2022-07-18 10:07:25 -0500

    CREATE ACTIVITY

--- app/src/main/java/pe/edu/ulima/pm/uset/CreateProfileActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/uset/CreateProfileActivity.kt
@@ -1,38 +1,36 @@
 package pe.edu.ulima.pm.uset
 
-import android.app.Activity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import pe.edu.ulima.pm.uset.Fragments.CreateProfile.CreateProfile01Fragment
-import pe.edu.ulima.pm.uset.Fragments.Login.Login01Fragment
-import pe.edu.ulima.pm.uset.databinding.ActivityChatBinding
 import pe.edu.ulima.pm.uset.databinding.ActivityCreateProfileBinding
-import pe.edu.ulima.pm.uset.databinding.ActivityLoginBinding
+import pe.edu.ulima.pm.uset.toolbar.ToolbarLogin
 
 class CreateProfileActivity : AppCompatActivity() {
+    //VIEW BINDING FOR ACTIVITIESs
     private lateinit var binding : ActivityCreateProfileBinding
+    //FRAGMENT MANAGEMENT
     private val fragmentManager = supportFragmentManager
     private val fragmentTransaction = fragmentManager.beginTransaction()
     private val fragmentLoginList: List<Fragment> = listOf(CreateProfile01Fragment())
 
-
+    //ACTIVITY LIFECYCLE
     override fun onCreate(savedInstanceState: Bundle?) {
+        //VIEW BINDING FOR ACTIVITIES
         super.onCreate(savedInstanceState)
         binding = ActivityCreateProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        title = "...USET..."
+        //TOOLBAR
+        ToolbarLogin().show(this,"USET",false,binding.tbToolbarlogin.mtToolbarlogin)
+        //FRAGMENT MANAGEMENT
         SetFragmentTransactionInTop(0)
     }
 
+    //FRAGMENT MANAGEMENT
     private fun SetFragmentTransactionInTop(fragment: Int) {
         fragmentTransaction
-            .add(R.id.fragmentContainerViewCreateProfile, fragmentLoginList[fragment],"1")
+            .add(R.id.fragmentContainerViewCreateProfile, fragmentLoginList[fragment])
             .commit()
     }
-
-
 }
